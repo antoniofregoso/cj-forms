eb93cc50ab6691a4f748b2873a911076c4168159
antoniofregoso <antoniofregoso@protonmail.com>
2025-07-03 16:37:02 -0600

    [IMP] FormAppoinment

--- src/components/FormAppoiment.js
+++ src/components/FormAppoiment.js
@@ -22,6 +22,11 @@ export class FormAppoinment extends FormLead {
             description:{
                 disabled:true
             }
+        },
+        appoinment:{
+            initialTime:9,
+            finalTime:17,
+            deltaTime:60
         }
     }
 
@@ -37,10 +42,49 @@ export class FormAppoinment extends FormLead {
      attributeChangedCallback(name, oldValue, newValue) {
         switch (newValue){
             case 'open':
-                const calendar = new Calendar('#calendar');
-                calendar.init()
+                const calendar = new Calendar('#calendar',{
+                    locale: this.state.context.lang,
+                    onClickDate(self, event) {
+                        let selection = event.target.parentNode;
+                        let date = selection.dataset.vcDate;
+                        const [year, month, day] = fecha.split('-');
+                        console.log(selection.dataset.vcDate)
+                    },
+                    });
+                calendar.init();
                 this.querySelector('.modal').classList.toggle('is-active');
                 break;
+        }
+    }
+
+    #pad(num) {
+    return num.toString().padStart(2, '0');
+  }
+
+    #getTimes(){
+        let times = '';
+        let deltaTime = this.state.appoinment.deltaTime;
+        let currentMinutes = this.state.appoinment.initialTime * 60;
+        const endMinutes = this.state.appoinment.finalTime * 60 + deltaTime;
+        if (currentMinutes<endMinutes){
+            while (currentMinutes <= endMinutes) {
+                const hours = Math.floor(currentMinutes / 60);
+                const minutes = currentMinutes % 60;
+                const timeStr = `${this.#pad(hours)}:${this.#pad(minutes)}`;
+            currentMinutes += deltaTime;
+            times += `<div class="cell"><button class="button is-small is-time" data-time="${timeStr}" disabled>${timeStr}</button></div>`
+        }
+        }else{
+            console.warn("finalTime must be greater than initialTime. It is expressed as integers in 24-hour format.")
+        }
+        return times
+    }
+
+    addTimeEvents(){
+        let options = this.querySelectorAll('.is-time');
+        for (let opt in options){
+            
+           console.log(opt)
         }
     }
 
@@ -59,6 +103,11 @@ export class FormAppoinment extends FormLead {
                             <div id="calendar"></div>
                         </div>
                         <div class="pt-2">
+                            <div class="fixed-grid has-6-cols">
+                                <div class="grid">
+                                    ${this.#getTimes()}
+                                </div>
+                            </div>
                         </div>
                         <div class="pt-2">
                             ${this.state?.form!=undefined?new CjForm(this.state.form, this.state.context).render():''}
